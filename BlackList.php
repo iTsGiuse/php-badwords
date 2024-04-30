7652db044e03b56fdd9f9f1e5707b9d4ed0832e8
iTsGiuse <giuse.amato@icloud.com>
2024-04-30 17:41:54 +0200

    feat: add first milestone

--- BlackList.php
+++ BlackList.php
@@ -11,12 +11,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Saluto</title>
+    <title>BlackList</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <header>
-        <h1 class="text-center">Saluto</h1>
+        <h1 class="text-center">BlackList</h1>
     </header>
     <main>
         <div class="container">
